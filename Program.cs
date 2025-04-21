cfdad65c05bd8605ac6a8e62f37c5733e009eb5e
gabs1san <sanbiel398@gmail.com>
2025-04-21 17:15:08 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFixacao1 {
+    class Program {
+        static void Main(string[] args) {
+
+            Student[] vect = new Student[10];
+
+
+            Console.Write("How many rooms will be retend? = ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++) {
+
+                Console.WriteLine();
+                Console.WriteLine("$ Rent #{i}:");
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new Student(name, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Occupied rooms: ");
+            for (int i = 0; i < 10; i++) {
+                if (vect[i] != null) {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExercicioFixacao1 {
+
+    class Student {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Student(string name, string email) {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString() {
+            return Name + ", " + Email;
+        }
+    }
+}
